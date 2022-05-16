c44a13e357c3daf5fb71b9704fd57dcefd8a01bc
Tony Fischetti <tony.fischetti@gmail.com>
2022-05-16 03:49:55 -0400

    almost there

---
+++ Gulpfile.js
@@ -0,0 +1,143 @@
+
+'use strict';
+
+const { series, parallel } = require('gulp');
+
+const fs = require("fs");
+const cheerio = require("cheerio");
+const axios = require("axios");
+const spinner = require("ora-promise")
+const md5 = require("md5");
+const $ = require("shelljs");
+
+// --------------------------------------------------------------- //
+
+const DOGS = {
+  "URL": "https://data.cityofnewyork.us/api/views/nu7n-tubp/rows.csv",
+  "MD5": "f214985644c8b2ade4f8cdc30667aa3e",
+  "LOC": "./data/dogs.csv",
+  "DES": "Dog License Data from NYC Open Portal"
+};
+
+const XWALK = {
+  "URL": "https://www.nycbynatives.com/nyc_info/new_york_city_zip_codes.php",
+  "MD5": "02bd1d96a157e20c0268a0d2351fefcf",
+  "LOC": "./data/zip-boro-xwalk.csv",
+  "DES": "Zip <-> Borough data from nycbynatives.com"
+};
+
+// --------------------------------------------------------------- //
+
+
+
+// --------------------------------------------------------------- //
+
+// CLEAN
+// MAKE STRUCTURE
+
+const setupDirs = (cb) => {
+  $.mkdir("-p", "data");
+  $.mkdir("-p", "target");
+  return cb();
+};
+
+
+/* ---------------------------------------------------------------
+ *
+ * This are the targets that download the data sources
+ * and place them in './data'
+ *
+ */
+
+const downloadDogData = (cb) => {
+  if (fs.existsSync(DOGS.LOC)){
+    console.log(`already have ${DOGS.DES}`);
+    return cb();
+  }
+  return spinner(`downloading ${DOGS.DES}`,
+    () => axios.get(DOGS.URL)).
+    then(resp => fs.promises.writeFile(DOGS.LOC, resp.data)).
+    catch(err => console.error(`failure: ${err}`));
+};
+
+const downloadZipBoroXwalk = (cb) => {
+  if (fs.existsSync(XWALK.LOC)){
+    console.log(`already have ${XWALK.DES}`);
+    return cb();
+  }
+
+  const writer = fs.createWriteStream(XWALK.LOC, { flags: 'w' });
+
+  return spinner(`downloading ${XWALK.DES}`,
+    () => axios.get(XWALK.URL)).
+    then(resp => cheerio.load(resp.data)).
+    catch(err => console.error(`failure: ${err}`)).
+    then($ => {
+      const zips = $("td:nth-child(1), td:nth-child(4)");
+      const boros = $("td:nth-child(2), td:nth-child(5)");
+      writer.write(`zip,boro\n`);
+      zips.each((i, e) => {
+        writer.write(`${$(e).text().trim()},${$(boros[i]).text().trim()}\n`);
+      });
+    });
+};
+
+
+/* ---------------------------------------------------------------
+ *
+ * This are the targets that download the data sources
+ * and place them in './data'
+ *
+ */
+
+const checkDogData = (cb) => {
+  return fs.promises.readFile(DOGS.LOC).
+    then(buf => {
+      if (md5(buf) !== DOGS.MD5){
+        throw Error(`Unexpected change in ${DOGS.LOC}`);
+      } else {
+        console.log("Hash of Dog Data is as expected");
+      }
+    });
+};
+
+// TODO make dry-er
+const checkZipBoroXwalk = (cb) => {
+  return fs.promises.readFile(XWALK.LOC).
+    then(buf => {
+      if (md5(buf) !== XWALK.MD5){
+        throw Error(`Unexpected change in ${XWALK.LOC}`);
+      } else {
+        console.log("Hash of Zip <-> Borough crosswalk is as expected");
+      }
+    });
+};
+
+// --------------------------------------------------------------- //
+
+const analyzeDogData = (cb) => {
+  $.exec("Rscript ./analyze-dog-data.R");
+  cb();
+};
+
+
+// --------------------------------------------------------------- //
+
+
+
+
+// the "download" target doesn't have to be "series" (as opposed
+// to "parallel") but there's a race condition in TTY output if
+// it's not "series"
+
+
+exports.setup     = setupDirs;
+exports.download  = series(downloadDogData, downloadZipBoroXwalk);
+exports.check     = parallel(checkDogData, checkZipBoroXwalk);
+exports.analyze   = analyzeDogData;
+
+exports.default   = series(exports.setup,
+                           exports.download,
+                           exports.check,
+                           exports.analyze);
+
---
+++ analyze-dog-data.R
@@ -0,0 +1,129 @@
+#!/usr/local/bin/Rscript --vanilla
+
+
+# ------------------------------ #
+rm(list=ls())
+
+options(echo=TRUE)
+options(width=80)
+options(warn=2)
+options(scipen=10)
+options(datatable.prettyprint.char=50)
+options(datatable.print.class=TRUE)
+options(datatable.print.keys=TRUE)
+options(datatable.na.strings="")
+
+
+require(colorout)
+library(data.table)
+library(magrittr)
+library(libbib)
+library(lubridate)
+
+# ------------------------------ #
+
+
+dogs <- fread("./data/dogs.csv")
+
+# remove/rename unecessary columns
+dogs <- dogs[, .(breed=BreedName, ZipCode, LicenseIssuedDate)]
+
+# the crosswalk between zip codes and boros
+xwalk <- fread("./data/zip-boro-xwalk.csv")
+xwalk <- xwalk[!duplicated(xwalk)]
+setnames(xwalk, "zip", "ZipCode")
+
+setkey(dogs, "ZipCode")
+setkey(xwalk, "ZipCode")
+
+# enrich the dog data with non-NA boroughs by merging
+# with the crosswalk
+dogs %<>% merge(xwalk, all.x=TRUE, by="ZipCode")
+
+# s/Staten$/Staten Island/g
+dogs[boro=="Staten", boro:="Staten Island"]
+
+# parse the MM/DD/YYYY date into a POSIX date
+dogs[, issued_date:=mdy(LicenseIssuedDate)]
+dogs[, LicenseIssuedDate:=NULL]
+
+# Let's get the number of licenses issues for each year!
+# 2014 and 2021 are incomplete, so we'll exclude those
+dogs[, .(licenses_issued=.N), year(issued_date)][
+  order(year)][
+  year <= 2020 & year >= 2015] %>% fwrite("./target/dog-licenses-by-year.csv")
+
+
+
+
+
+# Ok, we want to know each "Borough's Dog Breed"
+# Note that it isn't 'the most popular breed in each Borough...
+
+dogs[breed!="Unknown" & !is.na(boro)][
+  , .N, .(boro, breed)][
+  order(-N)][
+  !duplicated(boro)]
+
+# The most popular breed in all boroughs is Yorkshire Terrier (except
+# Staten Island [Shih Tzu])
+
+# Instead of the most popular breed, for each Borough, we'll choose the
+# breed that's most unique to each Borough
+# By this I mean: what's the Breed, in each Borough, that's the most
+# popular compared to all other Boroughs.
+
+# For example, Dalmatians aren't a incredibly popular breed, but,
+# assuming it were the case that 90% of NYC's Dalmatians lived in
+# the Bronx, Dalmatians would be ~"Bronx's signature breed"
+
+# Statistically speaking, each Borough's "breed" would be the breed
+# with the highest residuals in a Chi-Square test of independence
+# of proportions between that Borough and all other Borough's
+
+
+
+# let's remove all NA boros and 'Unknown' breeds
+dogs <- dogs[breed!="Unknown" & !is.na(boro)]
+
+# Let's only use breeds that have >=5,000 occurences in NYC
+dogs[, .N, breed][N>=5000][, breed] -> BREEDS_TO_KEEP
+dogs <- dogs[breed %chin% BREEDS_TO_KEEP]
+dogs[, .N, .(boro, breed)][order(-N)][!duplicated(boro)]
+
+
+
+make_nice_contingency <- function(bad_contigency){
+  cols <- bad_contigency %>% dimnames %>% {.[[2]]}
+  ret <- data.table(breed=row.names(bad_contigency),
+                    one=bad_contigency[,1],
+                    two=bad_contigency[,2])
+  setnames(ret, c("breed", cols[1], cols[2]))
+  setorder(ret, -target)
+  return(ret[])
+}
+
+get_top_dog <- function(DT, targetboro){
+  DT[, tmp:=ifelse(boro==targetboro, "target", "nottarget")]
+  table(DT$breed, DT$tmp) %>%
+    prop.table(margin=2) %>%
+    chisq.test %>%
+    {.$residuals} -> tmp
+  tmp %<>% make_nice_contingency
+  return(tmp[1, breed])
+}
+
+# let's turn off warnings as errors (chisq is inexact test)
+options(warn=1)
+get_top_dog(dogs, "Bronx")
+get_top_dog(dogs, "Staten Island")
+get_top_dog(dogs, "Manhattan")
+get_top_dog(dogs, "Brooklyn")
+get_top_dog(dogs, "Queens")
+
+list("Bronx", "Manhattan", "Staten Island", "Queens", "Brooklyn") %>%
+  lapply(function(x){ data.table(boro=x, signature_breed=get_top_dog(dogs, x)) }) %>%
+  rbindlist %>%
+  fwrite("./target/boros-signature-breeds.csv")
+
+
